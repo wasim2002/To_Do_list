a1c5f904c06750c62f9c1a73ad96a8f147b4b1a5
wasim2002 <wansari4540@gmail.com>
2024-05-15 18:46:19 +0530

    write code for adding todo and display it on real time

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+window.addEventListener("load",updateList);
+function getCloudData() {
+    return JSON.parse(localStorage.getItem("todo")) || [];
+};
 const main = document.querySelector("main");
 
 main.innerHTML = `
 <div class="input">
     <div class="inputContainer">
-        <input type="text" id="input" placeholder="Add To-Do here...">
+        <input type="text" id="input" autocomplete="off" placeholder="Add To-Do here...">
         <button class="addBtn">Add</button>
     </div>
 </div>
@@ -27,20 +31,49 @@ const input = document.querySelector("#input");
 
 addBtn.addEventListener("click", () => {
     if (input.value != "") {
-        addTodo(input.value)
-    }
+        addTodo(input.value);
+        input.value = "";
+    };
 });
 
 function addTodo(e) {
-    let list = `<div class="list">
-<input type="checkbox" id="checkbox">
-<div class="todo">${e}</div>
-<div class="icons">
-    <i class="fa-solid fa-pen"></i>
-    <i class="fa-solid fa-trash-can"></i>
-</div>
-</div>`;
-    lists.insertAdjacentHTML("afterbegin", list);
-    input.value = ""
+    let newTodo = {
+        id: new Date().getTime(),
+        todo: e
+    };
+    setCloudData(newTodo);
+    getCloudData();
+    updateList();
 }
+function setCloudData(todo) {
+    let getData = getCloudData();
+    getData.push(todo);
+    localStorage.setItem("todo", JSON.stringify(getData));
+};
+function updateList() {
+    let allTodos = getCloudData()
+    lists.innerHTML="";
+    allTodos.forEach((todo) => {
+        let list = `<div class="list" id="${todo.id}">
+        <input type="checkbox" id="checkbox">
+        <div class="todo">${todo.todo}</div>
+        <div class="icons">
+            <i class="fa-solid fa-pen"></i>
+            <i class="fa-solid fa-trash-can"></i>
+        </div>
+        </div>`;
+        lists.insertAdjacentHTML("afterbegin", list);
+    })
+}
+// function addTodo(e) {
+//     let list = `<div class="list">
+// <input type="checkbox" id="checkbox">
+// <div class="todo">${e}</div>
+// <div class="icons">
+//     <i class="fa-solid fa-pen"></i>
+//     <i class="fa-solid fa-trash-can"></i>
+// </div>
+// </div>`;
+//     lists.insertAdjacentHTML("afterbegin", list);
+// }
 
